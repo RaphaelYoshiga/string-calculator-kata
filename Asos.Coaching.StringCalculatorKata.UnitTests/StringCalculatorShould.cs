15ca541f2b58e8b7796b71b0c76dfdd2e2dbf592
Raphael Yoshiga <Raphael.Yoshiga@asos.com>
2016-12-13 16:41:48 +0000

    First test, ResultInZeroWhenInputIsEmpty

---
+++ Asos.Coaching.StringCalculatorKata.UnitTests/StringCalculatorShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Asos.Coaching.StringCalculatorKata.UnitTests
+{
+    [TestFixture]
+    public class StringCalculatorShould
+    {
+        [Test]
+        public void ResultInZeroWhenInputIsEmpty()
+        {
+            int result = StringCalculator.Calculate("");
+
+            result.Should().Be(0);
+        }
+    }
+
+    internal class StringCalculator
+    {
+        public static int Calculate(string empty)
+        {
+            return 0;
+        }
+    }
+}
